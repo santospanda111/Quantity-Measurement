d0a5ab4bb1502d3c3d36d427c1ae14989ef56f6b
santospanda111 <santospanda111@gmail.com>
2021-06-20 23:19:45 +0530

    Added UC2 to convert feet to yard or inch to yard and vice-versa with testcases

--- main/quantity_measurement.py
+++ main/quantity_measurement.py
@@ -11,7 +11,9 @@ class Feet:
         if isinstance(other, Feet):
             return self.feet == other.feet
         elif isinstance(other, Inch):
-            return self.feet == other.inch/12
+            return self.feet == other.inch / 12
+        elif isinstance(other, Yard):
+            return self.feet == other.yard * 3
         return False
 
 
@@ -29,6 +31,27 @@ class Inch:
             return self.inch == other.inch
         elif isinstance(other, Feet):
             return self.inch == other.feet * 12
+        elif isinstance(other, Yard):
+            return self.inch == other.yard * 36
+        return False
+
+
+class Yard:
+    def __init__(self, yard):
+        self.yard = yard
+
+    def __eq__(self, other):
+        """
+        -Here i have used isinstance method will check whether the other value is inch or feet.
+        :param other:
+        :return:Boolean Value
+        """
+        if isinstance(other, Yard):
+            return self.yard == other.yard
+        elif isinstance(other, Feet):
+            return self.yard == other.feet / 3
+        elif isinstance(other, Inch):
+            return self.yard == other.inch / 36
         return False
 
 
--- test/test_quantity_measurement.py
+++ test/test_quantity_measurement.py
@@ -1,85 +1,121 @@
-from main.quantity_measurement import Feet, Inch
+from main.quantity_measurement import Feet, Inch, Yard
 
 """Tested the followings according to the testcase."""
 
 
-def test_givenZeroFtAndZeroFt_WhenCompared_ShouldReturnTrue():
+def test_given_zero_feet_and_zero_feet_when_compared_should_return_true():
     first_feet = Feet(0.0)
     second_feet = Feet(0.0)
     assert first_feet == second_feet
 
 
-def test_givenZeroFtAndNone_WhenCompared_ShouldReturnFalse():
+def test_given_zero_feet_and_none_when_compared_should_return_false():
     first_feet = Feet(0.0)
     assert first_feet is not None
 
 
-def test_givenTwoSameFeetValues_WhenComparedForReference_ShouldReturnTrue():
+def test_given_two_same_feet_values_when_compared_reference_should_return_true():
     first_feet = Feet(0.0)
     second_feet = first_feet
     assert first_feet == second_feet
 
 
-def test_givenZeroFtAndFloatValue_WhenCompared_ShouldReturnFalse():
+def test_given_zero_feet_and_float_value_when_compared_should_return_false():
     first_feet = Feet(0.0)
     second_feet = float(0.0)
-    assert  first_feet != second_feet
+    assert first_feet != second_feet
 
 
-def test_givenOneFtAndOneFt_WhenCompared_ShouldReturnTrue():
+def test_given_one_feet_and_one_feet_when_compared_should_return_true():
     first_feet = Feet(1.0)
     second_feet = Feet(1.0)
     assert first_feet == second_feet
 
 
-def test_givenZeroInchAndNone_WhenCompared_ShouldReturnFalse():
+def test_given_zero_inch_and_none_when_compared_should_return_false():
     first_inch = Inch(0.0)
     assert first_inch is not None
 
 
-def test_givenTwoSameInchValues_WhenComparedForReference_ShouldReturnTrue():
+def test_given_two_same_inch_values_when_compared_reference_should_return_true():
     first_inch = Inch(0.0)
     second_inch = first_inch
     assert first_inch == second_inch
 
 
-def test_givenZeroInchAndFloatValue_WhenCompared_ShouldReturnFalse():
+def test_given_zero_inch_and_float_value_when_compared_should_return_false():
     first_inch = Inch(0.0)
     second_inch = float(0.0)
-    assert  first_inch != second_inch
+    assert first_inch != second_inch
 
 
-def test_givenZeroInchAndZeroInch_WhenCompared_ShouldReturnTrue():
+def test_given_zero_inch_and_zero_inch_when_compared_should_return_true():
     first_inch = Inch(0.0)
     second_inch = Inch(0.0)
     assert first_inch == second_inch
 
 
-def test_givenZeroFeetAndZeroInch_WhenCompared_ShouldReturnTrue():
+def test_given_zero_feet_and_zero_inch_when_compared_should_return_true():
     first_feet = Feet(0.0)
     second_inch = Inch(0.0)
     assert first_feet == second_inch
 
 
-def test_givenOneInchAndOneFeet_WhenCompared_ShouldReturnFalse():
+def test_given_one_inch_and_one_feet_when_compared_should_return_false():
     first_feet = Feet(1)
     second_inch = Inch(1)
     assert first_feet != second_inch
 
 
-def test_givenOneFeetAndOneInch_WhenCompared_ShouldReturnFalse():
+def test_given_one_feet_and_one_inch_when_compared_should_return_true():
     first_inch = Inch(1)
     second_feet = Feet(1)
     assert first_inch != second_feet
 
 
-def test_givenOneFtAndTwelveInch_WhenCompared_ShouldReturnTrue():
+def test_given_one_feet_and_twelve_inch_when_compared_should_return_true():
     feet = Feet(1)
     inch = Inch(12)
     assert feet == inch
 
 
-def test_givenTwelveInchAndOneFt_WhenCompared_ShouldReturnTrue():
+def test_given_twelve_inch_and_one_feet_when_compared_should_return_true():
     inch = Inch(12)
     feet = Feet(1)
     assert inch == feet
+
+
+def test_given_three_feet_and_one_yard_when_compared_should_return_true():
+    feet = Feet(3)
+    yard = Yard(1)
+    assert feet == yard
+
+
+def test_given_one_feet_and_one_yard_when_compared_should_return_false():
+    feet = Feet(1)
+    yard = Yard(1)
+    assert feet != yard
+
+
+def test_given_one_inch_and_one_yard_when_compared_should_return_false():
+    inch = Inch(1)
+    yard = Yard(1)
+    assert inch != yard
+
+
+def test_given_one_yard_and_36_inch_when_compared_should_return_true():
+    yard = Yard(1)
+    inch = Inch(36)
+    assert yard == inch
+
+
+def test_given_36_inch_and_one_yard_when_compared_should_return_true():
+    inch = Inch(36)
+    yard = Yard(1)
+    assert inch == yard
+
+
+def test_given_one_yard_and_three_feet_when_compared_should_return_true():
+    yard = Yard(1)
+    feet = Feet(3)
+    assert yard == feet
